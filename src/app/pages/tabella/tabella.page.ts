7f44d6e4946534bc1bc20089ce1b2d3438fd7bd0
fede49 <federicademarco49@gmail.com>
2022-04-30 13:40:29 +0200

    aggiunta funzione arrotonda

--- src/app/pages/tabella/tabella.page.ts
+++ src/app/pages/tabella/tabella.page.ts
@@ -13,18 +13,21 @@ export class TabellaPage implements OnInit {
   miglia: number;
 
   constructor() { }
+  arrotonda(risultato:number):number{
+    return +risultato.toPrecision(3);
+  }
   
-  cambioCelsius(celsius:number): void {  
-    this.fahrenheit = ((this.celsius *1.8) + 32);
+  cambioCelsius(celsius: number): void {  
+    this.fahrenheit =this.arrotonda ((this.celsius *1.8) + 32);
   }
   cambioFahrenheit(fahrenheit: number): void {  
-    this.celsius = ((fahrenheit -32) * (5 / 9));
+    this.celsius =this.arrotonda ((fahrenheit -32) * (5 / 9));
   }
   cambioKm(kilometri: number): void {  
-    this.miglia = (kilometri* 0.6214);
+    this.miglia =this.arrotonda (kilometri* 0.6214);
   }
   cambioMi(miglia: number): void {  
-    this.kilometri = (miglia * 1.609);
+    this.kilometri =this.arrotonda (miglia * 1.609);
   }
 
   ngOnInit() {
